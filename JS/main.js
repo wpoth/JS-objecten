77bb39705d258e7f3c3aac70b90e908712c750d8
wpoth <302758116@student.rocmondriaan.nl>
2023-11-13 12:09:30 +0100

    opdracht 5

---
+++ JS/main.js
@@ -0,0 +1,76 @@
+// opdracht 1
+let myProfile = {
+  name: "Wesley",
+  lastName: "Poth",
+  age: 16,
+  study: "Software Developer",
+  score: "8",
+};
+
+// opdracht 2
+let faceBook = {
+  userName: "Wesley Poth",
+  titel: "Profile",
+  story:
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, ipsa!",
+  date: "13/11/2023",
+  views: 69,
+};
+
+const products = [
+  {
+    title: "Paprika chips",
+    description: "platte stukjes aardappel met paprika smaak",
+    healthy: "no",
+  },
+
+  {
+    title: "Tomato",
+    description: "een groot rond stuk groente",
+    healthy: "yes",
+  },
+
+  {
+    title: "Ananas",
+    description:
+      "Ananas heeft een zoete en pittige smaak met een subtiele scherpte die op de tong blijft hangen",
+    healthy: "yes",
+  },
+];
+
+// opdracht 4
+let car1 = {
+  title: "Porsche Cayenne 4.8 S Tiptronic S 2012 Zwart",
+  description:
+    "The Porsche Cayenne 4.8-liter V8 engine has a horsepower of 385 @ 6,200 rpm and a torque of 369 lb-ft @ 3,500 rpm1.",
+  price: "€ 19.950,-",
+  kilometer: "249.887 km",
+  buildDate: "2012",
+};
+
+let car2 = {
+  title: "Ford Kuga 1.5 ST",
+  description:
+    "De Ford Kuga staat hoger op de wielen dan de Ford Focus waar hij hetzelfde onderstel mee deelt. Ook ziet hij er stoerder uit. Sinds 2011 is de Kuga verkrijgbaar met vierwielaandrijving. Deze specificaties zijn karakteristiek voor een SUV. Ford biedt SUV’s in verschillende formaten aan. Met een tweedehands Kuga kies je voor het middelgrote model. Het is een eigentijdse ruime gezinsauto die lekker stuurt en prima presteert. Als je geïnteresseerd bent in gebruikte middelgrote SUV’s, doe je er goed aan een proefrit in een Ford Kuga occasion te maken. ",
+  price: "€ 16.499,-.",
+  kilometer: "91.572 km",
+  buildDate: "2018",
+};
+
+let car3 = {
+  title: "Land Rover Range Rover Sport 2.0 P400e 404 pk",
+  description:
+    "Achterbank verwarmd, Achteropkomend verkeer waarschuwing, Achteruitrijcamera (086FA), Alarmsysteem, Automatische dimmende achteruitkijkspiegel (031CG), Autonomous Emergency Braking, Cruise control",
+  price: "€ 62.945,"
+  kilometer: "120.070 km",
+  buildDate: "2019",
+};
+
+// opdracht 5
+
+letitem1 = {
+  title: "Danerolles croissants",
+  beschrijving: "240 g",
+  actie: "per blik 1.39",
+  prijs: 1.39,
+};
